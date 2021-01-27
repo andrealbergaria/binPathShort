c67a42c9819ac8d3924b2fc31b9324eca30572c0
andre albergaria coelho <andre1coelho@outlook.com>
2021-01-27 03:59:24 +0000

    TEST

---
+++ binPathShort/SecretKeyValues.java
@@ -0,0 +1,15 @@
+package binPathShort;
+
+import javax.crypto.spec.SecretKeySpec;
+
+public class SecretKeyValues extends SecretKeySpec {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
